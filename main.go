b15c4061259d016f55807f6e91229d517c3d2e22
corocn <crsk.plmzh@gmail.com>
2018-08-16 14:33:17 +0900

    add ascii art

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"github.com/MakeNowJust/heredoc"
 	"github.com/eliukblau/pixterm/ansimage"
 	"image/color"
-	"os"
 )
 
 func main() {
+	drawAsciiArtGorilla()
+	println("Oowa Oowa !!")
+}
+
+func drawTwemojiGorilla() {
 	var (
 		pix *ansimage.ANSImage
 		err error
@@ -21,10 +26,64 @@ func main() {
 		ansimage.ScaleModeResize,
 		ansimage.NoDithering)
 
-	if err != nil {
-		os.Exit(1)
+	if err == nil {
+		pix.Draw()
 	}
+}
 
-	pix.Draw()
-	println("Oowa Oowa !!")
+func drawAsciiArtGorilla() {
+	doc := heredoc.Doc(`
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMHkkbkkkkkkkkkkbbkkkHMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMkkkkkkkkkbkkkkkkkkkkkbkkkkkkkqMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkbkkkMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMkkkkkkkkkkkbkkkkkkkkkkkkkkkkkkbkkkkkbkkkbHMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMkkkkkkkkkkkkkkkkbkkbkkbkkbkkbkkkkkkkkkkkkkkkkkHMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMkkkkkkkkbkkbkkkkkkkkkkkkkkkkkkkkkkkkkbkkkkkkkkkkkkHMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMkkkkkkkkkkkkkkkkbkkkkkkkkkkkkkkkkkkbkkkkkkbkkbkkkkkkkkMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMkkkkkkkkkkkkkkkkkkkkbkkbkkbkkbkkbkkkkkkkkkkkkkkkkkkkkkkkHMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMkkkkkkkkkkkkbkkbkkkkkkkkkkkkkkkkkkkkkkkkkbkkkkkkkkkbkkkkkkkkMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMkkkkkkkkkkkkkkkkkkkbkkkkkkkkkkkkkkkkkkbkkkkkkbkkbkkkkkkkkkkkkkMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMkkkkkkkkkkkkkkkkkkkkkkkbkkbkkbkkbkkbkkkkkkkkkkkkkkkkkkkkkkkkkkkkMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMkkkkkkkkkkkkbkkbkkbkkkkkkkkkkkkkkkkkkkkkkkkbkkkkkkkkkbkkbkkkkkkkkkMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMkkkkkkkkkkkkkkkkkkkkkkbkkkkkkkkkkkkkkkkkbkkkkkkbkkbkkkkkkkkkkkkkkkkkMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMkkkkkkkkkbkkkkkkkkkkkkkkkkbkkbkkbkkbkkbkkkkkkkkkkkkkkkkkkkkkkbkkkkkkkkMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMHkkkkkkkkkkkkbkkbkkbkkkkkkkkkkkkkkkkkkkkkkkkkbkkkkkkkkkbkkbkkkkkkkkkkkkHMMMMMMMMMMMMMM
+MMMMMMMMMMMMMNkkkkkkkkkkkkkkkkkkkkkkbkkkkkkkkkkkkkkkkkkkbkkkkkbkkbkkkkkkkkkkkkkkkkkkkkHMMMMMMMMMMMMM
+MMMMMMMMMMMMMkkkkkkkkbkkbkkkkkkkkkkkkkkbkkbkkbkkbkkbkkkkkkkkkkkkkkkkkkkkkkkbkkbkkkkkkkkMMMMMMMMMMMMM
+MMMMMMMMMMMMMkkkkbkkkkkkkkkbkkbkkbkkkkkkkkkkkkkkkkkkkkkkkkkbkkkkkkkkbkkbkkkkkkkkkbkkkkkMMMMMMMMMMMMM
+MMMMMMMMHHMMHkkkkkkkkkkkkkkkkkkkkkkkbkkkkkkkkkkkkkkkkkbkkkkkkkbkkbkkkkkkkkkkkkkkkkkkkkkHMMHHMMMMMMMM
+MMMMHkk&+>zUbkkkkkkkbkkbkkkkkkkkkkkkkkkbkkbkkbkkbkkbkkkkkkkkkkkkkkkkkkkkkkbkkbkkkkkkkbkk9C>+&dkHMMMM
+MMMHkkkkkk>>?HkkbkkkkkkkkkbkkbkkbkkkkkkkkkkkkkkkkkkkkkkkkbkkkkkkkkkkbkkbkkkkkkkkbkkkkkH3>>dkkkkkHMMM
+MM#bkkkkkkI>>dkkkkkkkkHWUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUWHkkkkkkkk0>>jkkkkkkkMMM
+MM#kkkkkkkR>+dkkkkkkH6>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>;>>>>>>>vHkkkkkkk+;dkkkkkbkMMM
+MMNkkkkkkbHQWkkkkbkk0>>;>>>>;>>+&&&&&&&&&<>>>>>>>>>>>;>;>;>j&&&&&&&&+>>;>>;>;>>dkkbkkkbHkWkkkkkkkMMM
+MMMNkkkkkkkbkkkkkkkkI>>>>;>>>>+XkkkkkkkkkR>>;>;>;>;>>;>>;>dkbkkkkkkkHz>>;>>;>;>jkkkkkkkkkkkkbkkkHMMM
+MMMNkkkkkkkkkkkkkkkkS>>;>>;>>>+WkkkkkkkkkD>>>>>>>>>>>>;>>>dkkkkkkkkkHz>;>>;>>>;dkkkkkkkkkkkkkkkkMMMM
+MMMMNkkkkkkkkkbkkkkkR<>>>;>>;>>vHkkkkkkkK1>>;>>;>>;>>;>>;>?WkkkbkkkH6>>>;>>;>>+dkkkbkkkkkkkkkkkNMMMM
+MMMMMNkkkkkbkkkkkkkkHI>;>>>>>>>>+7UWHU9C>>>>>;>>>>>;>>>;>>>>?TWHW96<>>;>>;>>;>jWkkkkkkbkkbkkkkNMMMMM
+MMMMMMNHkkkkkkkkkbkkkR>>>;>>;>>>>>>>>>>>>>;>>>>>;>>>>;>>>>>;>>>>>>>>>;>>;>>;>>dkkkkkkkkkkkkkHNMMMMMM
+MMMMMMMNHkkkkkbkkkkkkk2>>;>>>;>>>>>>;<<!~'   '~<<>><<!''  '_!<;>>;>>;>>;>>;>>jkkkkkbkkkkkkkNMMMMMMMM
+MMMMMMMMMNkkkkkkkkkkkkR+>>>>>>;>;>><'             '            _<>;>>;>>>;>>+dkkkkkkkkbkkkHMMMMMMMMM
+MMMMMMMMMHkkkkkkkbkkkkkk>>;>>>>>>;:     ..JJJ-..    ...JJJ-.     (>;>>;>>;>>dkkkkkkkkkkkkkHMMMMMMMMM
+MMMMMMMMMHkkkbkkkkkkkkkf>>>;>;>>>;_   .jWkkkkkkc><(>+XkkbkkHc-    >>>;>>;>>>ZkkkkkbkkkkkkkHMMMMMMMMM
+MMMMMMMMMNkkkkkkkkkkkkK<>;>>>>;>>><...>zHkkkkkWz>>>>?WkkkkkH3>_..(>>;>>;>>;><4kkkkkkkbkkkkNMMMMMMMMM
+MMMMMMMMMNkkkkkkbkkkbHC>>;>>>>>>>>>>>>>>>?TTT1>>>>>>>>?7TTC<>>;;>>;>>;>>>;>>;jHkkkkkkkkkkkMMMMMMMMMM
+MMMMMMMMMNHkkbkkkkkkkD>;>>>>;>>;>>>;>;>>>>>>>>>>>>>>>;>>>>>>>>;>;>>;>>;>>;>>>>dkbkkkkkkkkHMMMMMMMMMM
+MMMMMMMMMMkkkkkkkkkkHC>>>;>>>>>>;>>>>>>>>>>;>>>>>;>>>>>>;>>;>>>>>;>>;>>;>>;>;>zbkkkbkkbkkkMMMMMMMMMM
+MMMMMMMMMMHkkkkkbkkkHz>;>>>;>>;>>>;>>;>>;>>>>>;>>>;>>;>;>;>>;;>;>>;>>;>>;>>;>;jWkkkkkkkkkNMMMMMMMMMM
+MMMMMMMMMMNkkkkkkkkkH<>;>>>>>>>>>>>>>>;>>>;>>>>>>>>>;>>>;>>;>>;>>;>>;>>;>>>>>>jWkkkkkkkkkMMMMMMMMMMM
+MMMMMMMMMMMNkkkkkkkkHz>>>;>>>>>>>;>>;>>>>>>;>>;>>;>>>;>>>>;>>>>>;>>>>>+>>>;>;>jkkkbkkbkkHMMMMMMMMMMM
+MMMMMMMMMMMNHkkkbkkkkZ>>>+dkkkkkbbWkkmmAAaaaa&&&&&&&&&&aaaaQAAQkkWbbkkkkkR<>>+dkkkkkkkkHMMMMMMMMMMMM
+MMMMMMMMMMMMNkkkkkkkkHc;>>+OTWHkkkkkkkbbkkkkkkkkkkkkkkkkkkkkkkkkkkkkkHW96<>>>jkbkkkkkkkMMMMMMMMMMMMM
+MMMMMMMMMMMMMNkkkkkkkkR+>>>>>>><17TTUUUUWWHHHHHHHHHHHHHHHHHWUU9UTTC1<>>>>>>>jbkkkkbkkkMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMNkkkkbkkkHx>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>;>>;>>;>>>>>>>>>>;+dbkkkkkkkkNMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMNHkkkkkbbkmx>>>>>>>>;>;>>;>>>>>>>>>>>>;>>;>>;>>;>;>;>>;>>+dWkkkkkkkkHMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMNHkbkkkkkkmx+>;>>;>>>>>>>;>;>>;>>;>>;>>>;>>;>>;>>;>;+&dbkkkkkkkkHNMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMNHkkkkkkkkbm&+>>>>>;>>>>>>;>>>>>>>>;>;>>;>>>>>>+udWkkkkkkkbkkNMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMNNkkkkkkkkkkHme&+>>;>>;>>>>;>>;>>>;>>;>++ugQWkkkkkkkkkkkNMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMNNHHkkkkkkkkkkkWkmA&&&&++++++&&&gQkWkkkkkkkkkkkkHHNMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNHHkkkkkkkkkkkkkkkkkkkbkkkkkkkkkkkqHHNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNNNNNNNNNNNNNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM`)
+	println(doc)
 }
